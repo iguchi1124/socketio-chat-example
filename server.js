3387dd6b5ba064c0d525f6e62dcd0acf08eff27a
Shota Iguchi <e.iguchi1124@gmail.com>
2016-05-03 12:26:23 +0900

    Files moved

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/client/index.html');
 });
 
 io.on('connection', function(socket){
